f8f6df6a28dcf7fac3afbd94d9ca3e833e68ee8a
Anam-Neupane <anamn0377@gmail.com>
2025-08-10 16:40:49 +0545

    Updates

---
+++ assets/index.js
@@ -0,0 +1,45 @@
+// Centralized asset imports to ensure Vite processes and rewrites URLs with correct base
+// Images
+import bgHero from './anime-character-near-galaxy-planet-illustration.jpg'
+import bgDragon from './anime-style-mythical-dragon-creature.jpg'
+import bgCity from './beautiful-anime-sakura-cityscape-cartoon-scene.jpg'
+import profile from './profile.jpg'
+import ring from './ring.png'
+import star from './star.png'
+import star1 from './star1.png'
+import rightArrow from './right-arrow.png'
+import menu from './menu.svg'
+import github from './github.svg'
+import linkedin from './linkedin.svg'
+import gmail from './gmail.svg'
+import twitter from './twitter.svg'
+import projectLogo1 from './project-logo1.png'
+import projectLogo2 from './project-logo2.png'
+import projectLogo3 from './project-logo3.png'
+
+// Docs/other
+import cvPdf from './Anam-Neupane_CV.pdf'
+import splash from './splash.png'
+
+export {
+  bgHero,
+  bgDragon,
+  bgCity,
+  profile,
+  ring,
+  star,
+  star1,
+  rightArrow,
+  menu,
+  github,
+  linkedin,
+  gmail,
+  twitter,
+  projectLogo1,
+  projectLogo2,
+  projectLogo3,
+  cvPdf,
+  splash,
+}
+
+
