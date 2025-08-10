f1ea63942061a137318bb421cc3b90eaff14b9b5
Anam-Neupane <anamn0377@gmail.com>
2025-08-10 18:23:09 +0545

    For domain

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Default to root for platforms like Vercel; override via CLI for GitHub Pages
+  // Serving at a custom domain root
   base: "/",
 })
